7355a6bfbb539e98d7f1dafc4cc4a3bba944f351
expldec <explosive.deconstruction@gmail.com>
2022-07-08 16:07:17 +0200

    added tag table migration, added tag model, added contraints, added pivot table

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            'notizie',
+            'recensioni',
+            'intel',
+            'amd',
+            'nvidia',
+            'corsair',
+            'fractal design',
+            'asus'
+        ];
+
+        foreach($tags as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($tag);
+            $new_tag->save();
+        }
+    }
+}
