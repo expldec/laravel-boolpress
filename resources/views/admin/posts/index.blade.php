0a0d179471ac233eeeb16fc9fc34fed30cd15fde
expldec <explosive.deconstruction@gmail.com>
2022-07-15 15:23:30 +0200

    added image in index view

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -12,7 +12,9 @@
             {{-- Single post --}}
             <div class="col">
                 <div class="card mb-3" style="width: 18rem;">
-                    {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
+                    @if ($post->cover)
+                        <img class="img-fluid" src="{{ asset('storage/' . $post->cover) }}" alt="">
+                    @endif
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="btn btn-primary">Leggi
